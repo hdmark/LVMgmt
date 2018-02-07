a086a9f0433f0c408d3ce63f7cee62d2bc329d6f
hdmark <mark.d.baskin@gmail.com>
2018-02-06 22:06:50 -0500

    r1

--- Dad/Profits.js
+++ Dad/Profits.js
@@ -21,15 +21,15 @@ function getProfitShowValue(val){
 		delete element;}
 	}
 	
-	for (var i = 0, length = radios.length; i < length; i++)
-	{
-	 if (radios[i].checked)
-	 {
-	  // do whatever you want with the checked radio
-	  alert(radios[i].value);
+	// for (var i = 0, length = radios.length; i < length; i++)
+	// {
+	//  if (radios[i].checked)
+	//  {
+	//   // do whatever you want with the checked radio
+	//   alert(radios[i].value);
 
-	  // only one radio can be logically checked, don't check the rest
-	  break;
-	 }
-	}
+	//   // only one radio can be logically checked, don't check the rest
+	//   break;
+	//  }
+	// }
 }
